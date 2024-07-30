0393abdfdeb7f1a10dce46b92be3788a02b0aab1
tymiao1220 <tymiao1220@gmail.com>
2024-07-30 11:52:58 -0400

    Adding sanitizer for the url

--- web_client/src/views/workflow1.vue
+++ web_client/src/views/workflow1.vue
@@ -181,6 +181,7 @@
 <script>
 import "blueimp-gallery/css/blueimp-gallery.min.css";
 import Gallery from "blueimp-gallery/js/blueimp-gallery.min.js";
+import { sanitizeUrl } from '@braintree/sanitize-url'
 
 const ws = process.env.VUE_APP_WEBSOCKET_URL;
 
@@ -231,8 +232,8 @@ export default {
     render() {
       this.processing = false;
       this.$api.base.record(this.$props['id']).then((e) => {
-          this.link = e.data.link;
-          this.lineFileLink = e.data.lineFileLink;
+          this.link = sanitizeUrl(e.data.link);
+          this.lineFileLink = sanitizeUrl(e.data.lineFileLink);
           this.size = e.data.size;
           this.numberOfRoIs = e.data.numberOfRoIs;
           this.processTime = e.data.numberOfRoIs.processTime;
